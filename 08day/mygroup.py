da042e084b9584ab091f84d1f3bffcf0ec804869
OCC111 <859899882@qq.com>
2018-08-16 16:30:37 +0800

    文件更新2018-8-16-16:30

--- 08day/mygroup.py
+++ 08day/mygroup.py
@@ -1,58 +1,61 @@
 import pygame
+import random
 SCREEN_RECT = pygame.Rect(0, 0, 480, 700)
-
+CREATE_ENEMY_EVENT = pygame.USEREVENT
 class GameSprite(pygame.sprite.Sprite):
-	def __init__(self,imagename,speed=1):
-		super().__init__()
-		self.image = pygame.image.load(imagename)
-		self.rect = self.image.get_rect()
-		self.speed = speed
+	def __init__(self):
+		# 1. 调用父类方法，创建敌机精灵，并且指定敌机的图像
+		#super().__init__("./images/images/enemy1.png")
+		# 2. 设置敌机的随机初始速度 1 ~ 3
+		self.speed = random.randint(1, 3)
+		# 3. 设置敌机的随机初始位置
+		self.rect.bottom = 0
+		max_x = SCREEN_RECT.width - self.rect.width
+		self.rect.x = random.randint(0,max_x)
 
 	def update(self):
 		self.rect.y += self.speed
 
 
 class EnemySprite(GameSprite):
-	def __init__(self):
-		self.imagename = "./images/images/enemy0.png"
-		super().__init__(self.imagename)
+	def __init__(self):        
+		super().__init__()
+		self.imagename = "./images/images/enemy1.png"
+
 
 	def update(self):
 		super().update()
+		# 2. 判断是否飞出屏幕，如果是，需要将敌机从精灵组删除
+		if self.rect.y >= SCREEN_RECT.height:
+		#print("敌机飞出屏幕...")   
+			self.kill()
 
 
-class BackGroundSprites(EnemySprite):
-    """游戏背景精灵"""
+class BackgroundSprite(GameSprite):
+	"""游戏背景精灵"""
+	def __init__(self,is_alt=False):
+	    self.imagename = "./images/images/background.png"
+	    super().__init__(self.imagename,10)
 
-    def update(self):
-
-        # 1. 调用父类的方法实现
-        super().update()
-
-        # 2. 判断是否移出屏幕，如果移出屏幕，将图像设置到屏幕的上方
-        if self.rect.y >= SCREEN_RECT.height:
-            self.rect.y = -self.rect.height
-            
-class Enemy(GameSprite):
-    """敌机精灵"""
-
-    def __init__(self):
-
-        # 1. 调用父类方法，创建敌机精灵，并且指定敌机的图像
-        super().__init__("./images/enemy1.png")
-
-        # 2. 设置敌机的随机初始速度
-
-        # 3. 设置敌机的随机初始位置
-
-    def update(self):
-
-        # 1. 调用父类方法，让敌机在垂直方向运动
-        super().update()
-
-        # 2. 判断是否飞出屏幕，如果是，需要将敌机从精灵组删除
-        if self.rect.y >= SCREEN_RECT.height:
-            print("敌机飞出屏幕...")    
+	    # 判断是否交替图片，如果是，将图片设置到屏幕顶部
+	    if is_alt:
+	        self.rect.y = -self.rect.height     
 
 
-		
+	def update(self):
+		# 1. 调用父类的方法实现
+		super().update()
+
+		# 2. 判断是否移出屏幕，如果移出屏幕，将图像设置到屏幕的上方
+		if self.rect.y >= SCREEN_RECT.height:
+			self.rect.y = -self.rect.height
+
+class HeroSprite(GameSprite):
+	def __init__(self):
+		self.imagename = "./images/images/hero.git"
+		super().__init__(self.imagename,0)
+		#settings 
+		self.rect.centerx = SCREEN_RECT.centerx
+		self.rect.top = 550
+	def update(self):
+		self.rect.x += self.speed
--- 08day/planman.py
+++ 08day/planman.py
@@ -1,6 +1,8 @@
 import pygame
 from mygroup import *
 CREATE_ENEMY_EVENT = pygame.USEREVENT
+
+
 class PlaneGame(object):
     def __init__(self):
         self.screen = pygame.display.set_mode(SCREEN_RECT.size)
@@ -8,12 +10,9 @@ class PlaneGame(object):
         self.__create_sprites()
         pygame.time.set_timer(CREATE_ENEMY_EVENT, 1000)
 
-        image_name = "./images/background.png"
-        super().__init__(image_name)
-
         # 判断是否交替图片，如果是，将图片设置到屏幕顶部
         if is_alt:
-            self.rect.y = -self.rect.height     
+            self.rect.y -= self.rect.height
 
     def start_game(self):
         while True:
@@ -34,24 +33,17 @@ class PlaneGame(object):
             pygame.display.update()
 
     def __create_sprites(self):
-        self.back_group = pygame.sprite.Group()
-        # 敌机组
-        self.enemy_group = pygame.sprite.Group()
-        # 英雄组
-        self.hero_group = pygame.sprite.Group()
         # 创建背景精灵和精灵组
-        bg1 = Background("./images/background.png")
-        bg2 = Background("./images/background.png")
-        bg2.rect.y = -bg2.rect.height
-        # 创建背景精灵和精灵组
-        bg1 = Background()
-        bg2 = Background(True)
-        self.enemy_group.update()
-        self.enemy_group.draw(self.screen)
-
-
+        #global bg1
+       # global bg2
+        bg1 = BackgroundSprite()
+        bg2 = BackgroundSprite(True)
+        bg2 = Background("./images/images/background.png")
         self.back_group = pygame.sprite.Group(bg1, bg2)
-
+        bg2.rect.y = -bg2.rect.height
+        self.enemy_group = pygame.sprite.Group()
+        self.hero = Hero()
+        self.hero_group = pygame.sprite.Group(self.hero)
 
     def __event_handler(self):
         """事件监听"""
@@ -61,37 +53,31 @@ class PlaneGame(object):
             if event.type == pygame.QUIT:
                 PlaneGame.__game_over()
             elif event.type == CREATE_ENEMY_EVENT:
-                print("敌机出场...")
-
+                # print("敌机出场...")
+                enemy = EnemySprite()
+                self.enemy_group.add(enemy)
 
     def __check_collide(self):
         """碰撞检测"""
-        self.back_group = pygame.sprite.Group()
-        # 敌机组
-        self.enemy_group = pygame.sprite.Group()
-        # 英雄组
-        self.hero_group = pygame.sprite.Group()
-
+        pass
 
     def __update_sprites(self):
-        """更新精灵组"""
-        for group in [self.back_group, self.enemy_group, self.hero_group]:
-            group.update()
-            group.draw(self.screen)
-
         self.back_group.update()
         self.back_group.draw(self.screen)
-
-
+        self.enemy_group.update()
+        self.enemy_group.draw(self.screen)
+        if event.type == CREATE_ENEMY_EVENT:
+            self.enemy_group.add(Enemy())
+        self.hero_group.update()
+        self.hero_group.draw(self.screen)
 
     @staticmethod
     def __game_over():
-       """游戏结束"""
+        """游戏结束"""
 
-       print("游戏结束")
-       pygame.quit()
-       exit()
-
+        print("游戏结束")
+        pygame.quit()
+        exit()
 
 
 if __name__ == "__main__":
